0e94af844cc2130dc6ff0a6e8f6c736fdc0dcae9
benreckas <benreckas@gmail.com>
2017-12-05 12:31:20 -0800

    Mostly fully functioning app. Still needs alot of refactoring.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,23 @@
-const userInput = document.querySelector('input');
+const userInput = document.querySelector('.list-input');
+const userEdit = document.querySelector('.list-edit');
+const modal = document.querySelector('.modal');
 const forms = document.forms;
-const todoList = document.querySelector('ul');
+const todoList = document.querySelector('.todo-list');
+const completedList = document.querySelector('.completed-list');
 const todoArr = [];
+const completedArr = [];
 
-const userSubmit = function() {
+// todo item constructor function
+function Todo(li, check, uncheck, del, edit) {
+  this.li = li;
+  this.check = check;
+  this.uncheck = uncheck;
+  this.del = del;
+  this.edit = edit;
+};
+
+
+const userSubmit = () => {
   forms.addToDo.submit.addEventListener('click', (e) => {
     e.preventDefault();
     const inputText = userInput.value;
@@ -12,9 +26,8 @@ const userSubmit = function() {
       addLi();
       userInput.value = "";
     }
-  })
+  });
 };
-
 userSubmit();
 
 const addLi = function() {
@@ -27,6 +40,33 @@ const addLi = function() {
   checkbox.classList.add("fa", "fa-square-o");
   li.textContent = todoArr[todoArr.length - 1];
   todoList.append(li);
+  li.append(checkbox, editIcon, delIcon);
+};
+
+const completedLi = function() {
+  const li = document.createElement('li');
+  const delIcon = document.createElement('i');
+  const editIcon = document.createElement('i');
+  const checkbox = document.createElement('i');
+  li.classList.add("strikethrough");
+  delIcon.classList.add("fa", "fa-pencil", 'hidden');
+  editIcon.classList.add("fa", "fa-trash-o");
+  checkbox.classList.add("fa", "fa-check-square-o");
+  li.textContent = completedArr[completedArr.length - 1];
+  completedList.append(li);
+  li.append(checkbox, editIcon, delIcon);
+};
+
+const insertLi = function() {
+  const li = document.createElement('li');
+  const delIcon = document.createElement('i');
+  const editIcon = document.createElement('i');
+  const checkbox = document.createElement('i');
+  delIcon.classList.add("fa", "fa-pencil");
+  editIcon.classList.add("fa", "fa-trash-o");
+  checkbox.classList.add("fa", "fa-square-o");
+  li.textContent = todoArr[todoArr.length - 1];
+  todoList.insertBefore(li, todoList.firstChild);
   li.append(checkbox, editIcon, delIcon);
 };
 
@@ -43,9 +83,9 @@ const eListen = function() {
       case "fa-check-square-o":
         uncheckTask(eTarget);
         break;
-      // case "fa-pencil":
-      //   editTask();
-      //   break;
+      case "fa-pencil":
+        editTask(eTarget);
+        break;
     }
   });
 };
@@ -54,34 +94,54 @@ eListen();
 
 const delTask = function (eTarget) {
   const delConf = confirm("This will permenately delete the task.\nAre you sure you want to do this?");
-  if(delConf === true) {
+  if(delConf === true && eTarget.parentNode.parentNode == todoList) {
     todoList.removeChild(eTarget.parentNode);
+  }
+  else if(delConf === true && eTarget.parentNode.parentNode == completedList) {
+    completedList.removeChild(eTarget.parentNode);
   }
 };
 
 const checkTask = function(eTarget) {
-  eTarget.classList.remove('fa-square-o');
-  eTarget.classList.add('fa-check-square-o');
-  eTarget.parentNode.classList.add('strikethrough');
-  eTarget.parentNode.lastChild.classList.add('hidden');
-    // for(let i = 0; i <= todoArr.length; i++) {
-    //   if(todoArr[i] === eTarget.parentNode.textContent) {
-    //     todoArr.splice(i, 1)
-    //     console.log(todoArr);
-    //   }
-    //   todoArr.push(eTarget.parentNode.textContent);
-    //   console.log(todoArr);
-    //   return;
-    // }
+    todoList.removeChild(eTarget.parentNode);
+    for(let i = 0; i <= todoArr.length; i++) {
+      if(todoArr[i] === eTarget.parentNode.textContent) {
+        todoArr.splice(i, 1)
+      }
+    }
+    completedArr.push(eTarget.parentNode.textContent);
+    completedLi();
 };
 
 const uncheckTask = function(eTarget) {
-  eTarget.classList.add('fa-square-o');
-  eTarget.classList.remove('fa-check-square-o');
-  eTarget.parentNode.classList.remove('strikethrough');
-  eTarget.parentNode.lastChild.classList.remove('hidden');
+  completedList.removeChild(eTarget.parentNode);
+  for(let i = 0; i <= completedArr.length; i++) {
+    if(completedArr[i] === eTarget.parentNode.textContent) {
+      completedArr.splice(i, 1)
+    }
+  }
+  todoArr.push(eTarget.parentNode.textContent);
+  insertLi();
 };
 
 const editTask = function(eTarget) {
-  eTarget
+  userEdit.setAttribute('placeholder', eTarget.parentNode.textContent);
+  modal.classList.remove('hidden');
+  forms.editToDo.submit.addEventListener('click', (e) => {
+    e.preventDefault();
+    if(userEdit.value.length === 0) {
+      eTarget.parentNode.innerHTML = eTarget.parentNode.innerHTML;
+      modal.classList.add('hidden');
+      userEdit.value = '';
+    } else {
+      eTarget.parentNode.innerHTML = `${userEdit.value} <i class="fa fa-square-o"></i><i class="fa fa-trash-o"></i><i class="fa fa-pencil"></i>`;
+      modal.classList.add('hidden');
+      userEdit.value = '';
+    }
+  });
+  document.addEventListener('click', (e) => {
+    if(e.target === modal) {
+      modal.classList.add('hidden');
+    }
+  });
 }

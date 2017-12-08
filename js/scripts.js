260dd92b1a894e79667ebffd84c96f903f7cc197
benreckas <benreckas@gmail.com>
2017-12-07 18:24:47 -0800

    Started refactoring. Still need to finish most of the function refactoring.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,23 @@
 // Queries
 const userInput = document.querySelector('.list-input');
 const userEdit = document.querySelector('.list-edit');
-const modal = document.querySelector('.modal');
+const editModal = document.querySelector('.edit-modal');
 const forms = document.forms;
 const todoList = document.querySelector('.todo-list');
 const completedList = document.querySelector('.completed-list');
 const todoArr = [];
 const completedArr = [];
 
-// todo item constructor function
-function Todo(li, check, uncheck, del, edit) {
-  this.li = li;
-  this.check = check;
-  this.uncheck = uncheck;
-  this.del = del;
-  this.edit = edit;
+function Todo(str) {
+  this.li = document.createElement('li');
+  this.li.textContent = str;
+  this.checkbox = document.createElement('i');
+  this.editIcon = document.createElement('i');
+  this.delIcon = document.createElement('i');
+  this.checkbox.classList.add("fa", "fa-square-o"); // Checkbox Icon
+  this.editIcon.classList.add("fa", "fa-pencil"); // Pencil Icon
+  this.delIcon.classList.add("fa", "fa-trash-o"); // Trash Icon
+  this.li.append(this.checkbox, this.delIcon, this.editIcon);
 };
 
 // Listen for user submission on form input, push it's value to todoArr,
@@ -25,24 +28,18 @@ const userSubmit = function() {
     const inputText = userInput.value;
     if(inputText.length > 1) {
       todoArr.push(inputText);
-      addLi();
+      addToDoLi();
       userInput.value = "";
     }
   });
 };
+
 userSubmit();
 
-const addLi = function() {
-  const li = document.createElement('li');
-  const delIcon = document.createElement('i');
-  const editIcon = document.createElement('i');
-  const checkbox = document.createElement('i');
-  delIcon.classList.add("fa", "fa-pencil");
-  editIcon.classList.add("fa", "fa-trash-o");
-  checkbox.classList.add("fa", "fa-square-o");
-  li.textContent = todoArr[todoArr.length - 1];
-  todoList.append(li);
-  li.append(checkbox, editIcon, delIcon);
+const addToDoLi = () => {
+  const str = todoArr[todoArr.length - 1];
+  const createNewTask = new Todo(str);
+  todoList.append(createNewTask.li);
 };
 
 const completedLi = function() {
@@ -72,78 +69,79 @@ const insertLi = function() {
   li.append(checkbox, editIcon, delIcon);
 };
 
-const eListen = function() {
-  document.addEventListener('click', (e) => {
-    const eTarget = e.target;
-    switch (eTarget.classList[1]) {
-      case "fa-trash-o":
-        delTask(eTarget);
-        break;
-      case "fa-square-o":
-        checkTask(eTarget);
-        break;
-      case "fa-check-square-o":
-        uncheckTask(eTarget);
-        break;
-      case "fa-pencil":
-        editTask(eTarget);
-        break;
-    }
-  });
-};
-
-eListen();
-
-const delTask = function (eTarget) {
+const delTask = function (targetLi) {
   const delConf = confirm("This will permenately delete the task.\nAre you sure you want to do this?");
-  if(delConf === true && eTarget.parentNode.parentNode == todoList) {
-    todoList.removeChild(eTarget.parentNode);
+  if(delConf === true && targetLi.parentNode == todoList) {
+    todoList.removeChild(targetLi);
   }
-  else if(delConf === true && eTarget.parentNode.parentNode == completedList) {
-    completedList.removeChild(eTarget.parentNode);
+  else if(delConf === true && targetLi.parentNode == completedList) {
+    completedList.removeChild(targetLi);
   }
 };
 
-const checkTask = function(eTarget) {
-    todoList.removeChild(eTarget.parentNode);
+const checkTask = function(targetLi) {
+    todoList.removeChild(targetLi);
     for(let i = 0; i <= todoArr.length; i++) {
-      if(todoArr[i] === eTarget.parentNode.textContent) {
+      if(todoArr[i] === targetLi.textContent) {
         todoArr.splice(i, 1)
       }
     }
-    completedArr.push(eTarget.parentNode.textContent);
+    completedArr.push(targetLi.textContent);
     completedLi();
 };
 
-const uncheckTask = function(eTarget) {
-  completedList.removeChild(eTarget.parentNode);
+const uncheckTask = function(targetLi) {
+  completedList.removeChild(targetLi);
   for(let i = 0; i <= completedArr.length; i++) {
-    if(completedArr[i] === eTarget.parentNode.textContent) {
+    if(completedArr[i] === targetLi.textContent) {
       completedArr.splice(i, 1)
     }
   }
-  todoArr.push(eTarget.parentNode.textContent);
+  todoArr.push(targetLi.textContent);
   insertLi();
 };
 
-const editTask = function(eTarget) {
-  userEdit.setAttribute('placeholder', eTarget.parentNode.textContent);
-  modal.classList.remove('hidden');
+const editTask = function(targetLi) {
+  userEdit.setAttribute('placeholder', targetLi.textContent);
+  editModal.classList.remove('hidden');
   forms.editToDo.submit.addEventListener('click', (e) => {
     e.preventDefault();
     if(userEdit.value.length === 0) {
-      eTarget.parentNode.innerHTML = eTarget.parentNode.innerHTML;
-      modal.classList.add('hidden');
+      targetLi.innerHTML = targetLi.innerHTML;
+      editModal.classList.add('hidden');
       userEdit.value = '';
     } else {
-      eTarget.parentNode.innerHTML = `${userEdit.value} <i class="fa fa-square-o"></i><i class="fa fa-trash-o"></i><i class="fa fa-pencil"></i>`;
-      modal.classList.add('hidden');
+      targetLi.innerHTML = `${userEdit.value} <i class="fa fa-square-o"></i><i class="fa fa-trash-o"></i><i class="fa fa-pencil"></i>`;
+      editModal.classList.add('hidden');
       userEdit.value = '';
     }
   });
   document.addEventListener('click', (e) => {
-    if(e.target === modal) {
-      modal.classList.add('hidden');
+    if(e.target === editModal) {
+      editModal.classList.add('hidden');
     }
   });
-}
+};
+
+// Change this to take in an event and and element
+const eventListeners = function(element, evt) {
+  element.addEventListener(evt, (e) => {
+    const targetLi = e.target.parentNode;
+    switch (e.target.classList[1]) {
+      case "fa-trash-o": // Delete Icon
+        delTask(targetLi);
+        break;
+      case "fa-square-o": // Empty Checkbox Icon
+        checkTask(targetLi);
+        break;
+      case "fa-check-square-o": // Checkbox Icon
+        uncheckTask(targetLi);
+        break;
+      case "fa-pencil": // Edit Icon
+        editTask(targetLi);
+        break;
+    }
+  });
+};
+
+eventListeners(document, 'click');
